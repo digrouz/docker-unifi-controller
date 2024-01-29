fd51e62e0bf78c3806a5b5493aa9b097c9cc33a8
DI GREGORIO Nicolas <nicolas.digregorio@gmail.com>
2024-01-29 16:44:19 +0100

    new workflow to update mongodb

---
+++ .github/scripts/update-mongo.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+MONGO_URL="https://www.mongodb.com/download-center/community/releases"
+
+LAST_VERSION=$(curl -SsL ${MONGO_URL} | \
+               grep -o -e 'https://repo.mongodb.org/apt/debian/dists/buster/mongodb-org/4.4/main/binary-amd64/mongodb-org-server_4.4.[0-9]\+_amd64.deb' | \
+               uniq | \
+               grep -o -e '[0-9]\+.[0-9]\+.[0-9]\+'
+              )
+
+if [ "${LAST_VERSION}" ];then
+  sed -i -e "s|MONGO_VERSION='.*'|MONGO_VERSION='${LAST_VERSION}'|" Dockerfile*
+fi 
+
+if output=$(git status --porcelain) && [ -z "$output" ]; then
+  # Working directory clean
+  echo "No new version available!"
+else 
+  # Uncommitted changes
+  git commit -a -m "Mongodb update to version: ${LAST_VERSION}"
+  git push
+fi
